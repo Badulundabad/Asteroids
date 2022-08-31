2f58d944b343410b67ab2a480897cac8046ac884
DMITRIY GRIN <greendmitriy407@gmail.com>
2022-08-31 13:36:11 +0300

    added AmmoLifeTimeChecker

---
+++ Assets/Scripts/Model/Services/AmmoLifeTimeChecker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Asteroids.Model.Services
+{
+    public class AmmoLifeTimeChecker
+    {
+        private List<Ammo> ammos;
+        public event Action<Ammo> OnLifeTimeExpired;
+
+        public AmmoLifeTimeChecker(List<Ammo> ammos)
+        {
+            this.ammos = ammos;
+        }
+
+        public void Check()
+        {
+            for (int i = 0; i < ammos.Count; i++)
+            {
+                Ammo ammo = ammos[i];
+                if (ammo.ShootTime < Time.time - ammo.MaxLifeTime)
+                {
+                    OnLifeTimeExpired?.Invoke(ammo);
+                }
+            }
+        }
+    }
+}
