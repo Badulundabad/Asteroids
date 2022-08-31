ec4e4aaa2b547e19f554b3994dc671721b8c177c
DMITRIY GRIN <greendmitriy407@gmail.com>
2022-08-31 12:50:10 +0300

    Create SpaceActionEventArgs.cs

---
+++ Assets/Scripts/Controllers/SpaceActionEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Asteroids.Controllers
+{
+    public class SpaceActionEventArgs
+    {
+        public readonly Vector2 position;       
+        public readonly Vector2 direction;       
+        public readonly Quaternion rotation;    
+        
+        public SpaceActionEventArgs(Vector2 position, Vector2 direction, Quaternion rotation)
+        {
+            this.position = position;
+            this.direction = direction;
+            this.rotation = rotation;
+        }
+    }
+}
