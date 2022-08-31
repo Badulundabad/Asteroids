33693359e79e5805fb9978b555bf28e3d7692d4a
DMITRIY GRIN <greendmitriy407@gmail.com>
2022-08-31 13:34:18 +0300

    changed Ammo
    renamed Init method to RegisterShootTime, for better understanding

--- Assets/Scripts/Model/Ammo.cs
+++ Assets/Scripts/Model/Ammo.cs
@@ -10,7 +10,7 @@ namespace Asteroids.Model
         public float MaxLifeTime { get => maxLifeTime; }
         public float ShootTime { get; private set; }
 
-        public void Init()
+        public void RegisterShootTime()
         {
             if (ShootTime == 0)
                 ShootTime = Time.time;
