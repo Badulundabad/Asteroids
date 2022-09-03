40661938218bc07a2670ca7ad784e2a80bd5e7ba
DMITRIY GRIN <greendmitriy407@gmail.com>
2022-09-03 17:30:02 +0300

    changed PlayerLaserController

--- Assets/Scripts/Controllers/Player/PlayerLaserController.cs
+++ Assets/Scripts/Controllers/Player/PlayerLaserController.cs
@@ -1,22 +1,53 @@
-﻿namespace Asteroids.Controllers
+﻿using Asteroids.Model;
+using Asteroids.Services;
+using Asteroids.View;
+using UnityEngine;
+
+namespace Asteroids.Controllers
 {
-    public class PlayerLaserController : IController
+    public class PlayerLaserController : BaseObjectController<Laser>
     {
-        public bool IsRunning { get; private set; }
+        private SpaceObject owner;
+        private AmmoLifeTimeChecker<Laser> lifeTimeChecker;
 
-        public void Start()
+        public PlayerLaserController(ISpaceObjectFactory<Laser> factory) : base(factory)
         {
-            throw new System.NotImplementedException();
+            lifeTimeChecker = new AmmoLifeTimeChecker<Laser>(objects);
+            lifeTimeChecker.OnLifeTimeExpired += (laser) => Destroy(laser);
         }
 
-        public void Stop()
+        public override void Update()
         {
-            throw new System.NotImplementedException();
+            if (!IsRunning) return;
+
+            lifeTimeChecker.Check();
+
+            if (owner == null) return;
+
+            foreach (var laser in objects)
+            {
+                Vector2 rotation = owner.Rotation * Vector3.up;
+                Vector2 position = owner.Position + (rotation * laser.Offset);
+                laser.SetPosition(position);
+                laser.SetRotation(owner.Rotation);
+            }
         }
 
-        public void Update()
+        public void Fire(SpaceActionEventArgs args)
         {
-            throw new System.NotImplementedException();
+            var projectile = factory.Create(args.position, args.direction, args.rotation, OnCollision);
+            projectile.SetLaunchTime(Time.time);
+            objects.Add(projectile);
+        }
+
+        public void SetLaserOwner(SpaceObject owner)
+        {
+            this.owner = owner;
+        }
+
+        private void OnCollision(SpaceObjectView whp, GameObject withwhom)
+        {
+
         }
     }
 }
