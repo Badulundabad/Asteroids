e8b8517e49019344025ec9c01711d7f36a2f7344
DMITRIY GRIN <greendmitriy407@gmail.com>
2022-09-03 18:58:37 +0300

    changed PlayerController. added laser charging

--- Assets/Scripts/Controllers/Player/PlayerController.cs
+++ Assets/Scripts/Controllers/Player/PlayerController.cs
@@ -8,19 +8,20 @@ using UnityEngine;
 
 namespace Asteroids.Controllers
 {
-    public class PlayerController : BaseObjectController<Ship>
+    public class PlayerController : BaseObjectController<PlayerShip>
     {
         private PlayerInput input;
         private PlayerShipMover shipMover;
-        private Ship ship;
+        private PlayerShip ship;
+        private LaserCharger laserCharger;
 
-        public event Action<Ship> OnPlayerSpawned;
+        public event Action<PlayerShip> OnPlayerSpawned;
         public event Action<SpaceActionEventArgs> OnDestroy;
         public event Action<SpaceActionEventArgs> OnGunFire;
         public event Action<SpaceActionEventArgs> OnLaserFire;
 
 
-        public PlayerController(ISpaceObjectFactory<Ship> factory) : base(factory)
+        public PlayerController(ISpaceObjectFactory<PlayerShip> factory) : base(factory)
         {
             input = new PlayerInput();
             input.Player.Fire1.performed += (context) => OnGunSlotFire();
@@ -34,6 +35,7 @@ namespace Asteroids.Controllers
             ship = factory.Create(Vector2.zero, Vector2.up, new Quaternion(0, 0, 0, 1), OnCollision);
             objects.Add(ship);
             shipMover.SetShip(ship);
+            laserCharger = new LaserCharger(ship);
             OnPlayerSpawned?.Invoke(ship);
             IsRunning = true;
         }
@@ -48,17 +50,24 @@ namespace Asteroids.Controllers
                 shipMover.Move(input.Player.Move.ReadValue<Vector2>());
             else
                 shipMover.Move(Vector3.zero);
+
+            laserCharger.Update();
         }
-        
+
         private void OnGunSlotFire()
         {
+            if (!ship.CanShoot()) return;
+
             ship.SetShotTimeNow();
             OnGunFire?.Invoke(new SpaceActionEventArgs(ship.Position, ship.Rotation * Vector2.up, ship.Rotation));
         }
 
         private void OnLaserSlotFire()
         {
-            ship.SetShotTimeNow();
+            if (ship.CurrentLaserCharges == 0f) return;
+
+            int charges = ship.CurrentLaserCharges - 1;
+            ship.SetLaserCharges(charges);
             OnLaserFire?.Invoke(new SpaceActionEventArgs(ship.Position, ship.Rotation * Vector2.up, ship.Rotation));
         }
 
