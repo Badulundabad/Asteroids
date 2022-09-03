3ca54667e247135375950ba2b1374d046ca2900d
DMITRIY GRIN <greendmitriy407@gmail.com>
2022-09-03 18:57:48 +0300

    added LaserCharger

---
+++ Assets/Scripts/Services/LaserCharger.cs
@@ -0,0 +1,37 @@
+﻿using Asteroids.Model;
+using UnityEngine;
+
+namespace Asteroids.Services
+{
+    public class LaserCharger
+    {
+        private PlayerShip ship;
+
+        public LaserCharger(PlayerShip ship)
+        {
+            this.ship = ship;
+        }
+
+        public void Update()
+        {
+            if (ship.CurrentLaserCharges >= ship.MaxLaserCharges) return;
+
+            float currentTime = ship.CurrentLaserChargingTimer;
+            if (ship.CurrentLaserChargingTimer == 0f)
+            {
+                currentTime = ship.LaserChargingTime;
+            }
+            else
+            {
+                currentTime -= Time.deltaTime;
+                if (currentTime < 0f)
+                {
+                    ship.SetLaserCharges(ship.CurrentLaserCharges + 1);
+                    currentTime = 0f;
+                }
+            }
+
+            ship.UpdateLaserChargingTime(currentTime);
+        }
+    }
+}
