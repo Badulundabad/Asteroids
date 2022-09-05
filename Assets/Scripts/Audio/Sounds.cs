8f4bec986d6ebaffc55beca88966e4bdfabf8634
DMITRIY GRIN <greendmitriy407@gmail.com>
2022-09-05 10:47:54 +0300

    added AudioPlayer and Sounds classes

---
+++ Assets/Scripts/Audio/Sounds.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Asteroids.Audio
+{
+    [CreateAssetMenu(fileName = "Sounds", menuName = "Asteroids/Lists/Sounds")]
+    public class Sounds : ScriptableObject
+    {
+        [SerializeField] private AudioClip buttonClick;
+        [SerializeField] private AudioClip gunShot;
+        [SerializeField] private AudioClip laser;
+        [SerializeField] private AudioClip explosion;
+        [SerializeField] private AudioClip music;
+
+        public AudioClip ButtonClick { get => buttonClick; }
+        public AudioClip GunShot { get => gunShot; }
+        public AudioClip Laser { get => laser; }
+        public AudioClip Explosion { get => explosion; }
+        public AudioClip Music { get => music; }
+    }
+}
